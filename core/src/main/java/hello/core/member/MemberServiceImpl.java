6e7b089ac6682756c283ce461624685881f960c9
ms-cho91 <thirdxero@jj.ac.kr>
2024-02-27 18:02:14 +0900

    컴포넌트 스캔(@ComponentScan)과 의존관계 자동 주입(@Autowired)

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
 // OCP, DIP 위반
@@ -7,6 +11,7 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
