c482538ef68ad0abf4eac897b83d2268ca44ad6b
ms-cho91 <thirdxero@jj.ac.kr>
2024-03-04 16:38:19 +0900

    빈스코프 프로토타입 스코프 / 싱글톤 빈과 프로토타입 빈 함께 사용 방법

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,50 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+
+        //스프링 빈이 종료메서드 실행 안함.
+        System.out.println("스프링 종료메서드 실행 요청");
+        ac.close();
+
+        System.out.println("PrototypeBean 종료메서드 실행 요청");
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.close");
+        }
+    }
+}
