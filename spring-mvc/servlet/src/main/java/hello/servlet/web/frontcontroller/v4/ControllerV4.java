a1f9ca4cab1830a467ed4544955609659ef77f8c
ms-cho91 <thirdxero@jj.ac.kr>
2024-03-18 15:44:42 +0900

    no message

--- spring-mvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
+++ spring-mvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -6,9 +6,9 @@ public interface ControllerV4 {
 
     /**
      *
-     * @param paraMap
+     * @param paramMap
      * @param model
      * @return viewName
      */
-    String process(Map<String, String> paraMap, Map<String, Object> model);
+    String process(Map<String, String> paramMap, Map<String, Object> model);
 }
