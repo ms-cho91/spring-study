8440647de414b5c579ad4847d5620f661da7afbe
ms-cho91 <thirdxero@jj.ac.kr>
2024-03-22 11:19:43 +0900

    PRG Post Redirect Get / RedirectAttributes

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -92,21 +93,39 @@ public class BasicItemController {
      * model.addAttribute(item); 자동 추가, 생략 가능
      * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
      */
-    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item) {
-        itemRepository.save(item);
-        return "basic/item";
-    }
+//    @PostMapping("/add")
+//    public String addItemV3(@ModelAttribute Item item) {
+//        itemRepository.save(item);
+//        return "basic/item";
+//    }
+
 
     /**
      * @ModelAttribute 자체 생략 가능
      * model.addAttribute(item) 자동 추가
      */
 //    @PostMapping("/add")
-    public String addItemV4(Item item) {
-        itemRepository.save(item);
-        return "basic/item";
+//    public String addItemV4(Item item) {
+//        itemRepository.save(item);
+//        return "basic/item";
+//    }
+
+//    @PostMapping("/add")
+//    public String addItemV5(@ModelAttribute Item item) {
+//        itemRepository.save(item);
+//        return "redirect:/basic/items/"+item.getId();
+//    }
+
+    @PostMapping("/add")
+    public String addItemV6(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedId = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", savedId.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
