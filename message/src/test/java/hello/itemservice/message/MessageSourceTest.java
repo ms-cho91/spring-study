5d825e0077ee173708be72490cb308cb1603bbad
ms-cho91 <thirdxero@jj.ac.kr>
2024-03-26 18:11:39 +0900

    다국어 처리 메세지 국제화

---
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,28 @@
+package hello.itemservice.message;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+
+        String result = ms.getMessage("hello", null, null);
+
+        System.out.println("MessageSourceTest.helloMessage");
+        System.out.println("result = " + result);
+
+        Assertions.assertThat(result).isEqualTo("안녕");
+
+    }
+
+}
