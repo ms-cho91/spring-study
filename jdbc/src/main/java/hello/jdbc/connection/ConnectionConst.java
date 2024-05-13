1d434109f033b8d66bbda547aa8f2ea50177d95f
ms-cho91 <thirdxero@jj.ac.kr>
2024-05-13 18:05:32 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/test_db";
+    public static final String USERNAME = "seong91";
+    public static final String PASSWORD = "root1234!";
+
+}
